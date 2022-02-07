ff46f2592a7362c5b12d240cff6960aa98f72fc0
arius-decimus <justynbryant6@gmail.com>
2022-02-07 14:36:48 -0800

    Add game function and refine computerPlay function
    Skip ahead and add game function.
    Refine computerPlay function to be lowercase
    while returning first letter uppercase string.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,31 @@
-const rpsArray = ['Rock', 'Paper', 'Scissors'];
+const rpsArray = ['rock', 'paper', 'scissors'];
+let result;
+let wins = 0;
+let losses = 0;
+let compChoice;
 
 function computerPlay () {
   const rng = Math.floor(Math.random() * rpsArray.length);
-  return (rng, rpsArray[rng])
+  compChoice = (rng, rpsArray[rng]);
+  compChoiceUpper = compChoice.charAt(0).toUpperCase() + compChoice.slice(1);
+  compChoice = compChoiceUpper;
+  return ('The computer chooses: ' + compChoice);
 }
 
-function rpsRound () {
-  
+
+
+function game () {
+  rpsRound
+  if (result = 'Player wins') {
+    wins++;
+  } else if (result = 'Player loses') {
+    losses++;
+  } else if (result = 'Tie') {
+    wins ++;
+    losses ++;
+  } else {
+    return 'Error in game';
+  }
+  return wins;
+  return losses;
 }
