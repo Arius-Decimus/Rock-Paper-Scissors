7ec24abe0ecffde4add4c421b07798f4b33bec55
arius-decimus <justynbryant6@gmail.com>
2022-02-08 13:26:12 -0800

    Add for loop version successfully

--- javascript.js
+++ javascript.js
@@ -3,42 +3,101 @@ let result;
 let wins = 0;
 let losses = 0;
 let compChoice;
+let playerChoiceRaw;
 let playerChoice;
-playerChoice = prompt('Rock-Paper-Scissors', 'Rock');
+let i = 0;
 
-function computerPlay () {
+/* function computerPlay () {
   const rng = Math.floor(Math.random() * rpsArray.length);
   compChoice = (rng, rpsArray[rng]);
   return ('The computer chooses: ' + compChoice);
-}
+} */
 
-function rpsRound () {
+/* function rpsRound () {
   if (compChoice == 'scissors' && playerChoice == 'rock' ||
   compChoice == 'rock' && playerChoice == 'paper' ||
   compChoice == 'paper' && playerChoice == 'scissors') {
     result = 'You win';
+    wins++;
   } else if (compChoice == 'scissors' && playerChoice == 'paper' ||
   compChoice == 'rock' && playerChoice == 'scissors' ||
   compChoice == 'paper' && playerChoice == 'rock') {
     result = 'You lose';
+    losses++;
   } else if (compChoice == 'scissors' && playerChoice == 'scissors' ||
   compChoice == 'rock' && playerChoice == 'rock' ||
   compChoice == 'paper' && playerChoice == 'paper') {
     result = 'You tie';
+    wins++;
+    losses++;
   } else {
     result = 'le error'
   }
-  return result
+  return result + '. ' + 'Your wins: ' + wins + ' Your losses: ' + losses
+} */
+
+while (wins < 5 || losses < 5 || i < 10) {
+
+  function input() {
+  playerChoiceRaw = prompt('Rock-Paper-Scissors', 'Rock');
+  playerChoice = playerChoiceRaw.toLowerCase();
+  let rng = Math.floor(Math.random() * rpsArray.length);
+  compChoice = (rng, rpsArray[rng]);
+  }
+
+  if (compChoice == 'scissors' && playerChoice == 'rock' ||
+  compChoice == 'rock' && playerChoice == 'paper' ||
+  compChoice == 'paper' && playerChoice == 'scissors') {
+    result = 'You win';
+    wins++;
+    i++;
+    compChoice = (rng, rpsArray[rng])
+  } else if (compChoice == 'scissors' && playerChoice == 'paper' ||
+  compChoice == 'rock' && playerChoice == 'scissors' ||
+  compChoice == 'paper' && playerChoice == 'rock') {
+    result = 'You lose';
+    losses++;
+    i++;
+    compChoice = (rng, rpsArray[rng])
+  } else if (compChoice == 'scissors' && playerChoice == 'scissors' ||
+  compChoice == 'rock' && playerChoice == 'rock' ||
+  compChoice == 'paper' && playerChoice == 'paper') {
+    result = 'You tie';
+    wins++;
+    losses++;
+    i++;
+    compChoice = (rng, rpsArray[rng])
+    if (wins == 5 || losses == 5) {
+    }
+  } else {
+    result = 'le error';
+    i++;
+    compChoice = (rng, rpsArray[rng])
+  }
+
+  console.log('Computer chose: ' + compChoice + '. Player chose: ' + playerChoice + '. ' +
+  result + '. ' + 'Your wins: ' + wins + ' Your losses: ' + losses);
+
+  if (wins == 5) {
+    let finalOutcome = 'You win! Good day sir!';
+    console.log(finalOutcome);
+    break;
+  } else if (losses == 5) {
+    let finalOutcome = 'You lose! Good day sir!';
+    console.log(finalOutcome);
+    break;
+  } else if (wins == 5 && losses == 5) {
+    finalOutcome = 'You tie! Good day sir!';
+    console.log(finalOutcome);
+    break;
+  } else if (i == 10) {
+  let finalOutcome = 'Exceeded maximum number of attempts! Good day sir!';
+    console.log (finalOutcome);
+    break;
+}
 }
 
-switch (x) {
-  case ('blah'):
-  case ('bleh'):
-    console.log(lol)
-}
-
-
-function game () {
+/* function game () {
   if (result == 'You win') {
     wins++;
   } else if (result = 'You lose') {
@@ -50,4 +109,4 @@ function game () {
     console.log ('Error in game function');
   }
   return 'Your wins: ' + wins + ' Your losses: ' + losses;
-}
+} */
